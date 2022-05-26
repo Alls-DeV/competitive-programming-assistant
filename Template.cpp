56030ece842ead201fc24a99b31d0ec6d93a0482
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-05-26 13:15:53 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -3,9 +3,9 @@
 #include <ext/pb_ds/tree_policy.hpp>
 
 #ifdef ALE
-#define db(x) cout << #x << " = ", _print(x), cout << nl
+#include "/home/alls/Library/debug.h"
 #else
-#define db(x)
+#define dbg(...)
 #endif
  
 using namespace std;
@@ -31,28 +31,10 @@ using vpll = vector<pll>;
 #define ALL(x) x.begin(), x.end()
 #define PB push_back
 #define MP make_pair
-#define make_unique(x) {sort(all(x)); x.erase(unique(all(x)), x.end());}
+#define make_unique(x) {sort(ALL(x)); x.erase(unique(ALL(x)), x.end());}
 #define uid(a, b) uniform_int_distribution<int>(a, b)(rng) 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-void _print(int x) {cout << x;}
-void _print(ll x) {cout << x;}
-void _print(unsigned long long x) {cout << x;}
-void _print(double x) {cout << x;}
-void _print(ld x) {cout << x;}
-void _print(bool x) {cout << (x ? "true" : "false");}
-void _print(char x) {cout << x;}
-void _print(string x) {cout << x;}
-template<class T> using V = vector<T>;
-template<typename T> using ordered_set = __gnu_pbds::tree<T, __gnu_pbds::null_type, less<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-template<typename K, typename V> using ordered_map = __gnu_pbds::tree<K, V, less<K>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
-template <class T1, class T2> void _print(pair<T1, T2> p) {cout << "("; _print(p.first); cout << " "; _print(p.second); cout << ")";}
-template <class T> void _print(vector<T> v) {cout << "[ "; for (T x : v) {_print(x); cout << " ";} cout << "]";}
-template <class T> void _print(set<T> s) {cout << "{ "; for (T x : s) {_print(x); cout << " ";} cout << "}";}
-template <class T> void _print(multiset<T> s) {cout << "{ "; for (T x : s) {_print(x); cout << " ";} cout << "}";}
-template <class T> void _print(ordered_set<T> s) {cout << "{ "; for (T x : s) {_print(x); cout << " ";} cout << "}";}
-template <class T1, class T2> void _print(map<T1, T2> m) {cout << "{ "; for (auto x : m) {_print(x); cout << " ";} cout << "}";}
- 
 const int MOD = 1000000007;
 const long double eps = 5e-8;
 const char nl = '\n';
@@ -71,7 +53,7 @@ int32_t main() {
 	cin >> t;//testcase
     while (t--) {
         #ifdef ALE
-            cout << "------------------------------\n";
+            cout << "\e[36m" << "------------------------------\n" << "\e[39m";
         #endif
         solve();
     }
