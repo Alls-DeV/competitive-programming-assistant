0d26acf28e451ae20139a9b2e5eee5325efa4a0a
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-05-28 13:32:16 +0200

    Update stress.sh

--- scripts/stress.sh
+++ scripts/stress.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+RED='\033[1;31m'
+PURPLE='\033[1;35m'
+NC='\033[0m' # no color
+
 if [ "$1" == "-h" ] ; then
-    echo "stress.sh  wrong  slow  generator  numTests"
+    echo -e "${PURPLE}stress.sh  wrong  slow  generator  numTests${NC}"
 	echo ""
-	echo "remove //testcase from generator"
+	echo -e "${RED}REMOVE //testcase${NC}"
     exit 0
 fi
 for ((testNum=0;testNum<$4;testNum++))
@@ -15,16 +19,15 @@ do
     H2=`md5sum outSlow`
     if !(cmp -s "outWrong" "outSlow")
     then
-        echo "Error found!"
-        echo "Input:"
+        echo "${RED}Error found!${NC}"
+        echo "${PURPLE}Input:${NC}"
         cat input
-        echo "Wrong Output:"
+        echo "${PURPLE}Wrong Output:${NC}"
         cat outWrong
-        echo "Slow Output:"
+        echo "${PURPLE}Slow Output:${NC}"
         cat outSlow
         exit
     fi
 done
 echo Passed $4 tests
-
 
