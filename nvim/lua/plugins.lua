59c108dd28d6986d70c1a5579f38bc38da72aa3e
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-08-21 15:16:50 +0200

    Update plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -21,4 +21,10 @@ return require('packer').startup(function()
             'nvim-lualine/lualine.nvim',
             requires = { 'kyazdani42/nvim-web-devicons', opt = true }
         }
+        
+        -- Autopairs
+        use 'windwp/nvim-autopairs'
+        
+        -- Comment
+        use 'numToStr/Comment.nvim'
 end)
