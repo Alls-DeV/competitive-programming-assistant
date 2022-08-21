289ab0cd2308b597f72601702fa2dc724c632b5b
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-08-21 12:54:45 +0200

    Update and rename treesitter.lua to _treesitter.lua

---
+++ nvim/lua/_treesitter.lua
@@ -0,0 +1,11 @@
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = {
+    enable = true
+  }
+}
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
