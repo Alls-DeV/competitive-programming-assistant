44ec00c743989073262d8133348adb248891fecb
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-08-23 13:55:08 +0200

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -12,14 +12,11 @@ using ll = long long; using vi = vector<int>;
 #define ssize(x) (int)(x).size()
 #define ALL(x) x.begin(), x.end()
 #define rALL(x) x.rbegin(), x.rend()
-#define MU(x) {sort(ALL(x)); x.erase(unique(ALL(x)), x.end());}
-#define uid(a, b) uniform_int_distribution<int>(a, b)(rng) 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 const int MOD = 1000000007;
 const char nl = '\n';
  
 //////////////////// DO NOT TOUCH BEFORE THIS LINE ////////////////////////
- 
+
 void solve() {
 }
  
@@ -27,9 +24,10 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int t = 1;
 	cin >> t;//remove if single testcase
-    while (t--) {
+    FOR (i, 1, t+1) {
+        //cout << "Case #" << i << ": ";
         #ifdef ALE
-		cout << "\033[48;5;196m\033[38;5;15m" << "                                     \n" << "\033[0m";
+			cout << "\033[48;5;196m\033[38;5;15m" << "                                     \n" << "\033[0m";
         #endif
         solve();
     }
