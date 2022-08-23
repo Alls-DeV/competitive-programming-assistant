84bf1f32a72258ee1e2b80944ab6d2891ffa49ec
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-08-23 13:39:55 +0200

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-g++ -std=gnu++20 -Wall -Wextra -g -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -O2 -DALE -o $1.exe $1.cpp
+g++ -std=gnu++20 -Wall -Wextra -g -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -O2 -o $1.exe $1.cpp
