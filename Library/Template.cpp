3f4de50dfc8901331de36949de1c6b0d24a01ac2
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-10-31 08:45:04 +0100

    Update Template.cpp

--- Library/Template.cpp
+++ Library/Template.cpp
@@ -7,12 +7,9 @@
 using namespace std;
 //#define int long long
 using ll = long long; using vi = vector<int>;
-#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define rFOR(i, a, b) for (int i = (a); i >= (b); --i)
 #define ssize(x) (int)(x).size()
 #define ALL(x) x.begin(), x.end()
 #define rALL(x) x.rbegin(), x.rend()
-const int MOD = 1000000007;
 const char nl = '\n';
  
 //////////////////// DO NOT TOUCH BEFORE THIS LINE ////////////////////////
@@ -24,10 +21,10 @@ int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int t = 1;
 	cin >> t;//remove if single testcase
-    FOR (i, 1, t+1) {
+    for (int i = 0; i <= t; i++) {
         //cout << "Case #" << i << ": ";
         #ifdef ALE
-			cout << "\033[48;5;196m\033[38;5;15m" << "                                     \n" << "\033[0m";
+			cout << "\033[48;5;196m" << "                                     \n" << "\033[0m";
         #endif
         solve();
     }
