930815e36a56cf3d7e6264eaf4e23b554aa28308
Alessio De Vito <93343041+alls-cpp@users.noreply.github.com>
2022-10-31 08:46:49 +0100

    Delete nvim directory

--- nvim/lua/_treesitter.lua
+++
@@ -1,8 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
