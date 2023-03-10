c7750d8d3083ce8bc4c1571ee188599845debe7d
Alessio De Vito <alessiodevito73@gmail.com>
2023-03-10 21:02:41 +0100

    refactoring

--- cpa_debug.py
+++ cpa_debug.py
@@ -7,36 +7,88 @@ class colors:
     GREEN = "\033[0;32m"
     NC    = "\033[0m" # no color
 
-def debug(k = False, testcase = -1, A = False):
+def single_testcase(testcase, folder, inp, out, ans):
+    print("Testcase", testcase)
+    TLE = False
+    t = th.Thread(target = lambda: os.system(f"./{folder} < {inp} > {out}"))
+    t.start()
+    t.join(5)
+    if t.is_alive():
+        TLE = True
+        os.system(f"killall {folder}")
+
+    # check if the output is correct excluding white space and endline redirecting the diff output to a file
+    os.system(f"diff -B -i -w {ans} {out} > diff.out")
+    if os.path.getsize("diff.out") == 0 and TLE == False:
+        print("---------------")
+        print("Input")
+        os.system(f"cat {inp}")
+        print("---------------")
+        print("Output")
+        os.system(f"cat {out}")
+        print("---------------")
+        print(f"{colors.GREEN}Passed!{colors.NC}")
+        return 1
+    else:
+        # for each line in $out and in $ans, if they are different, print out in $FAILURE color and ans in $SUCCESS color
+        print("---------------")
+        print("Input")
+        os.system(f"cat {inp}")
+        print("---------------")
+        print("Output")
+        if TLE:
+            print("---------------")
+            print(f"{colors.RED}Failed! (Time limit exceeded){colors.NC}")
+            return 0
+        outFile = open(out, "r")
+        ansFile = open(ans, "r")
+        out_new_text = ""
+        ans_new_test = ""
+        for line_out, line_ans in zip(outFile, ansFile):
+            if line_out.strip() != line_ans.strip():
+                out_new_text += colors.RED + line_out + colors.NC
+                ans_new_test += colors.GREEN + line_ans + colors.NC
+            else:
+                out_new_text += line_out
+                ans_new_test += line_ans
+        outFile.close()
+        ansFile.close()
+        print(out_new_text, end = "")
+        print("---------------")
+        print("Expected")
+        print(ans_new_test, end = "")
+        print("---------------")
+        print(f"{colors.RED}Failed!{colors.NC}")
+        return 0
+
+def debug(k = False, testcase = -1, DEBUG = False):
     folder = os.getcwd()[os.getcwd().rfind('/') + 1:]
 
     # remove the executable file if it exists
     if os.path.exists(folder):
         os.system(f"rm -rf {folder}")
     
-    print("Compiling...")
-    
-    if k or testcase != -1:
         os.system(f"g++ -std=gnu++20 -Wall -Wextra -g -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -O2 -DDEBUG -o {folder} {folder}.cpp")
+    if DEBUG:
     else:
         os.system(f"g++ -std=gnu++20 -Wall -Wextra -g -fsanitize=address -fsanitize=undefined -fno-sanitize-recover -O2 -o {folder} {folder}.cpp")
 
     # check if the compilation is successful
     if not os.path.exists(folder):
         return
-
-    print(f"{colors.GREEN}Compilation successful{colors.NC}")
     
     if k:
         print("Enter your input manually, press ctrl+D to finish your input")
-        print("\nInput")
+        print("---------------")
+        print("Input")
         os.system("cat > tmp.txt")
-        print("\nOutput")
+        print("---------------")
+        print("Output")
         t = th.Thread(target = lambda: os.system(f"./{folder} < tmp.txt"))
         t.start()
         t.join(5)
         if t.is_alive():
-            print(f"{colors.RED}Time limit exceeded{colors.NC}")
+            print(f"{colors.RED}Failed! (Time limit exceeded){colors.NC}")
             os.system(f"killall {folder}")
         os.system("rm -rf tmp.txt")
 
@@ -53,58 +105,16 @@ def debug(k = False, testcase = -1, A = False):
             if not os.path.exists(inp):
                 break
             total += 1
-            TLE = False
-            t = th.Thread(target = lambda: os.system(f"./{folder} < {inp} > {out}"))
-            t.start()
-            t.join(5)
-            if t.is_alive():
-                TLE = True
-                os.system(f"killall {folder}")
-
-            # check if the output is correct excluding white space and endline redirecting the diff output to a file
-            os.system(f"diff -B -i -w {ans} {out} > diff.out")
-            if os.path.getsize("diff.out") == 0:
-                passed += 1
-                print(f"\n{colors.GREEN}Testcase {testcase} passed{colors.NC}")
-                # if -A is passed print testcase
-                if A:
-                    print("\nInput")
-                    os.system(f"cat {inp}")
-                    print("\nOutput")
-                    os.system(f"cat {out}")
-            else:
-                # for each line in $out and in $ans, if they are different, print out in $FAILURE color and ans in $SUCCESS color
-                if TLE:
-                    print(f"\n{colors.RED}Testcase {testcase} failed (Time limit exceeded){colors.NC}")
-                else:
-                    print(f"\n{colors.RED}Testcase {testcase} failed{colors.NC}")
-                print("\nInput")
-                os.system(f"cat {inp}")
-                print("\nOutput")
-                # open out and ans and compare line by line, if they are different, print out in $RED color and ans in $GREEN color
-                out = open(out, "r")
-                ans = open(ans, "r")
-                for line_out, line_ans in zip(out, ans):
-                    if line_out != line_ans:
-                        if len(line_out) + 10 > 70:
-                            line_to_print = colors.RED + line_out.strip() + colors.GREEN + '\n' + line_ans.strip() + colors.NC
-                        else:
-                            line_to_print = colors.RED + line_out.strip() + colors.GREEN + ' ' * 10 + line_ans.strip() + colors.NC
-                        print(line_to_print)
-                    else:
-                        print(line_out, end="")
-                out.close()
-                ans.close()
+            passed += single_testcase(testcase, folder, inp, out, ans)
+            print('\n')
 
         print()
         os.system("rm -rf diff.out")
-        #  do the same for the summary
         if passed == total:
-            print(f"{colors.GREEN}All testcases passed {colors.NC}")
-        elif total - passed == 1:
-            print(f"{colors.RED}{total - passed} testcase failed {colors.NC}")
+            c = colors.GREEN
         else:
-            print(f"{colors.RED}{total - passed} testcases failed {colors.NC}")
+            c = colors.RED
+        print(f"{c}{passed} / {total} passed!{colors.NC}")
 
     else:
         testcase = str(testcase)
@@ -114,18 +124,4 @@ def debug(k = False, testcase = -1, A = False):
         if not os.path.exists(inp):
             print(f"{colors.RED}Testcase {testcase} does not exist{colors.NC}")
             return
-        t = th.Thread(target = lambda: os.system(f"./{folder} < {inp} > {out}"))
-        t.start()
-        t.join(5)
-        if t.is_alive():
-            print(f"{colors.RED}Time limit exceeded{colors.NC}")
-            os.system(f"killall {folder}")
-            os.system(f"rm -rf {out}")
-            return
-        print("\nInput")
-        os.system(f"cat {inp}")
-        if A:
-            print("\nAnswer")
-            os.system(f"cat {ans}")
-        print("\nOutput")
-        os.system(f"cat {out}")
+        single_testcase(testcase, folder, inp, out, ans)
